19af2dc236a759e0758d9d0343a676013733dcbf
jhonatan <jhonatan@homebrasil.com>
2022-03-11 14:10:28 -0300

    Feat: nos-somos

---
+++ nos.php
@@ -0,0 +1,19 @@
+<?php include "content/include/head.php"; ?>	
+
+<section class="nos-somos">
+    <h1>
+        Contamos com pessoas sensacionais<br>
+        <span>em nosso time</span> 
+    </h1>
+    <p>
+        Além disso utilizamos as melhores tecnologias<br>
+        disponíveis no mercado. Se não existir a gente cria!
+    </p>
+</section>
+
+<section class="standard">
+
+</section>
+
+
+<?php include ('content/include/rodape.php'); ?>	
